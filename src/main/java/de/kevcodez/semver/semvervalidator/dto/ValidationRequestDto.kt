ab449cc34248d89fad02925809ce8efe42ab244f
Kevin <k.grueneberg1994@gmail.com>
2017-07-16 14:15:30 +0200

    Remove Lombok

--- src/main/java/de/kevcodez/semver/semvervalidator/dto/ValidationRequestDto.kt
+++ src/main/java/de/kevcodez/semver/semvervalidator/dto/ValidationRequestDto.kt
@@ -1,12 +1,5 @@
 package de.kevcodez.semver.semvervalidator.dto
 
-import lombok.AllArgsConstructor
-import lombok.Data
-import lombok.NoArgsConstructor
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 class ValidationRequestDto(var versionRange: String, var versionsToValidate: List<String>) {
 
 }
--- src/main/java/de/kevcodez/semver/semvervalidator/dto/VersionDto.kt
+++ src/main/java/de/kevcodez/semver/semvervalidator/dto/VersionDto.kt
@@ -2,15 +2,6 @@ package de.kevcodez.semver.semvervalidator.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 class VersionDto(var major: Int, var minor: Int, var patch: Int, var preRelease: String, var normal: String, var buildMetadata: String) {
 
